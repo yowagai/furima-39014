9c2b5e0f51150ce2753385d19b58362d57c0e8f2
yowagai <imlf4btbw28@gmail.com>
2023-02-21 06:46:44 +0900

    new,createアクション

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,21 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    Item.new
+  end
+
+  def create
+    @items = Item.create(item_params)
+  end
+
+private
+
+def item_params
+  params.require(:item).permit(:name, :price, :explanation :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :estimated_id, :image).merge(user_id: current_user.id)
+end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :items, only: [:index, :show, :destroy]
+  resources :items
 
 end
