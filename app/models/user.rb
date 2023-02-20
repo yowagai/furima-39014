3a3dd538b4d10e32a04380a68550cb095b615371
yowagai <imlf4btbw28@gmail.com>
2023-02-20 14:31:05 +0900

    半角英数字必須

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,7 @@ class User < ApplicationRecord
   validates :family_name_kana, presence: true
   validates :first_name_kana, presence: true
   validates :birthday, presence: true
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX,}
 end
