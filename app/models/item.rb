aa8ab48b91da7b4bf50d3e29ea45e822a3eab8bf
yowagai <imlf4btbw28@gmail.com>
2023-02-20 23:06:55 +0900

    アソシエーション、バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,21 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  # has_one :order
+  has_one_attached :image
+
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :explanation, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :delivery_charge_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :estimated_id, presence: true
+
+  validates :image, presence: true, unless: :was_attached?
+
+  def was_attached?
+    self.image.attached?
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -20,4 +20,7 @@ class User < ApplicationRecord
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX}
+
+  has_many :items
+  # has_many :orders
 end
