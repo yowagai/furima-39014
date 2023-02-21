5c03b7d0e8582dbdb828ad87465c8dcf53ba5ee4
yowagai <imlf4btbw28@gmail.com>
2023-02-21 16:55:07 +0900

    数値バリデーション、エラーハンドリング

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,8 +10,12 @@ class Item < ApplicationRecord
   # has_one :order
   has_one_attached :image
 
+  with_options presence: true, format: { with: /\A[0-9]+\z/ } do
+    validates :price, numericality: { onyl_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  end
+
+
   validates :name, presence: true
-  validates :price, presence: true
   validates :explanation, presence: true
   validates :category_id, presence: true, numericality: { other_than: 1 }
   validates :condition_id, presence: true, numericality: { other_than: 1 }
