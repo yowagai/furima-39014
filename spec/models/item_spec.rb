e2a53b2517bbbc07e93124be76294e680541aa77
yowagai <imlf4btbw28@gmail.com>
2023-02-22 07:40:01 +0900

    テストコード記入

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Item, type: :model do
 
   describe '商品出品' do
     context '新規商品出品できる場合' do
-      it "name、price、explanation、categry_id、condition_id、charge_id、prefecture_id、estimated_id、user_id、imageが存在すれば登録できる" do
+      it "name、price、explanation、categry_id、condition_id、charge_id、prefecture_id、estimated_id、imageが存在すれば登録できる" do
         expect(@item).to be_valid
       end
     end
@@ -17,6 +17,72 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Name can't be blank")
       end
+      it "priceが空では登録できない" do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it "explanationが空では登録できない" do
+        @item.explanation = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Explanation can't be blank")
+      end
+      it "category_idが空では登録できない" do
+        @item.category_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it "condition_idが空では登録できない" do
+        @item.condition_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
+      end
+      it "charge_idが空では登録できない" do
+        @item.charge_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Charge can't be blank")
+      end
+      it "prifectur_idが空では登録できない" do
+        @item.prefecture_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it "estimated_idが空では登録できない" do
+        @item.estimated_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Estimated can't be blank")
+      end
+      it "imageが空では登録できない" do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it "userが紐づいていないと登録できない" do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
+      end
+      it "priceが300円以下では登録できない" do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+      it "priceが9999999円以上では登録できない" do
+        @item.price = 10000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
+      it "priceが半角数値以外では登録できない" do
+        @item.price = '３００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+
+      
+
+
+
+
     end
   end
 end
