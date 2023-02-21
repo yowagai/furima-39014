3d484a51d0903df6c9fa06ee67fa70f5450fc1a3
yowagai <imlf4btbw28@gmail.com>
2023-02-22 06:32:48 +0900

    faker設定

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :item do
-    
+    name          {Faker::Lorem.words}
+    price         {Faker::Number.between(from: 300, to: 9999999)}
+    explanation   {Faker::Lorem.sentence}
+    category_id   {Faker::Number.between(from: 2, to: 11)}
+    condition_id  {Faker::Number.between(from: 2, to: 7)}
+    charge_id     {Faker::Number.between(from: 2, to: 3)}
+    prefecture_id {Faker::Number.between(from: 1, to: 47)}
+    estimated_id  {Faker::Number.between(from: 2, to: 4)}
   end
 end
