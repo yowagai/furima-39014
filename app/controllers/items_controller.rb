09bdb885bda46e2309731cea825557e9ead52f10
yowagai <imlf4btbw28@gmail.com>
2023-02-23 07:53:53 +0900

    本人のみ編集可

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,9 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    unless @item.user == current_user
+      redirect_to  root_path
+    end
   end
 
   def update
