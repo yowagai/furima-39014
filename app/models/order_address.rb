6e44db42d9058afc55a81b95790d23ca8f7bd14b
yowagai <imlf4btbw28@gmail.com>
2023-02-24 07:46:58 +0900

    データをテーブルに保存する処理

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -9,4 +9,10 @@ class OrderAddress
   validates :phone_number, presence: true
   validates :user_id, presence: true
   validates :item_id, presence: true
+
+  def save
+    order = Order.create(item_id: item_id, user_id: user_id)
+
+    Address.create(post_code: post_code, prefecture_id: prefecture_id, city: city, address: address, apartment: apartment, phone_number: phone_number, oedre_id: order.id)
+  end
 end
