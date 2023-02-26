9f5f693de630c55a9488a6fe4d626df536e3a358
yowagai <imlf4btbw28@gmail.com>
2023-02-27 07:14:35 +0900

    商品購入
    トークン発行から決済完了まで

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,23 +4,31 @@ class OrdersController < ApplicationController
     @order_address = OrderAddress.new
   end
 
-  def new
-  end
-
   def create
-    @order_address = OrderAddress.new(order_params)
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new(order_address_params)
     if @order_address.valid?
+      pay_order
       @order_address.save
-      redirect_to root_path
+      redirect_to root_path(@item)
     else
-      render :new
+      render :index
     end
   end
 
   private
 
-  def order_params
-    params.require(:order_address).permit(:post_code, :prefecture_id, :city, :address, :apartment, :phone_number).merge(item_id: @item.id, user_id: current_user.id)
+  def order_address_params
+    params.require(:order_address).permit(:post_code, :prefecture_id, :city, :address, :apartment, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_order
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_address_params[:token],
+      currency: 'jpy'
+    )
   end
 
 end
