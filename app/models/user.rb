16cc694f68a4475b60825d8f734abab7b7d7262a
yowagai <imlf4btbw28@gmail.com>
2023-03-01 08:17:43 +0900

    うまくいかなかったため元に戻す

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,6 +20,6 @@ class User < ApplicationRecord
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX }
 
-  has_many :items, dependent: :destroy
-  has_many :orders, dependent: :destroy
+  has_many :items
+  has_many :orders
 end
